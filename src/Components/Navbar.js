0273dce4d7823011f5abc5fdabc8d2c453e2380e
bitrish <bitu01061999@gmail.com>
2022-05-31 19:08:14 +0530

    new commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,7 +6,7 @@ export default class Navbar extends Component {
         return (
             <div style={{display:'flex',padding:'0.2', backgroundColor:'#925ac7'}}>
                 <Link to="/" style={{textDecoration:'none',color:'white'}}><h1 style={{marginTop:'0.3rem',marginLeft:'1rem'}}>Movies App</h1></Link>
-                <Link to="/fav" style={{textDecoration:'none',color:'white'}}><h2 style={{marginLeft:'2rem',marginTop:'0.8rem'}}>Favourites</h2></Link>
+                <Link to="/fav" style={{textDecoration:'none',color:'white'}}><h2 style={{marginLeft:'2rem',marginTop:'0.8rem'}}>Favourite</h2></Link>
             </div>
         )
     }
